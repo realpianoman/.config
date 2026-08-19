b92e6cb834edfc1a01f8a9c12826625368b3e9d5
luciendberkeley <lucienduchateau@students.berkeley.net>
2026-08-18 19:18:45 -0700

    Fixed the copy command in vim to keep cursor position

--- laptop/.config/nvim/lua/pianoman/remap.lua
+++ laptop/.config/nvim/lua/pianoman/remap.lua
@@ -1,4 +1,8 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>pc", vim.cmd.Alpha)
-vim.keymap.set("n", "<leader>ss", 'ggVG"+y')
+vim.keymap.set("n", "<leader>ss", function()
+    local pos = vim.api.nvim_win_get_cursor(0)
+    vim.cmd('normal! ggVG"+y')
+    vim.api.nvim_win_set_cursor(0, pos)
+end, { desc = "Yank entire file to system clipboard" })
